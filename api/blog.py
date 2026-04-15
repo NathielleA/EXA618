9c6d0c6ac8e4b25061354a9072a83fd69d491dc9
Nathielle Alves <nalves@ecomp.uefs.br>
2026-04-14 23:21:25 -0300

    Implement blog API handler with GET and PUT methods; add vercel.json configuration

--- api/blog.py
+++ api/blog.py
@@ -1,21 +1,61 @@
 import os
 import json
 from datetime import datetime
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
-# MONGODB_URI = os.environ.get('MONGODB_URI')
+MONGODB_URI = os.environ.get('MONGODB_URI')
 
-# def get_collection():
-#     if not MONGODB_URI:
-#         raise Exception("MONGODB_URI não definida")
+def get_collection():
+    if not MONGODB_URI:
+        raise Exception("MONGODB_URI não definida")
 
-#     client = MongoClient(MONGODB_URI)
-#     db = client['blogdb']
-#     return db['posts']
+    client = MongoClient(MONGODB_URI)
+    db = client['blogdb']
+    return db['posts']
 
 
 def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "API funcionando"
-    }
+    try:
+        collection = get_collection()
+
+        if request.method == "GET":
+            posts = list(collection.find({}, {'_id': 0}))
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"posts": posts})
+            }
+
+        elif request.method == "PUT":
+            data = json.loads(request.body)
+
+            autor = data.get("autor")
+            mensagem = data.get("mensagem")
+
+            if not autor or not mensagem:
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps({"error": "Campos obrigatórios"})
+                }
+
+            collection.insert_one({
+                "autor": autor,
+                "mensagem": mensagem,
+                "data": datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+            })
+
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"status": "ok"})
+            }
+
+        return {
+            "statusCode": 405,
+            "body": json.dumps({"error": "Método não permitido"})
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+
